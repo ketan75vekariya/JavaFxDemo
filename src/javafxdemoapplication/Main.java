f190ecfcb54eb68c88dd1774cc7dc6cf54cd0220
ketan75vekariya <ketan75vekariya@gmail.com>
2024-03-27 16:39:25 +0000

    Add Javafx Scene Builder and create new Main.fxml file.

--- src/javafxdemoapplication/Main.java
+++ src/javafxdemoapplication/Main.java
@@ -2,34 +2,19 @@ package javafxdemoapplication;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		Button btn =new Button();
-		Button exit =new Button();
-		btn.setText("Say Hello World");
-		exit.setText("Cancle");
-		exit.setOnAction(e->{
-			System.out.println("Closed the window");
-			System.exit(0);}); 
-		btn.setOnAction(new EventHandler<ActionEvent>(){
-			@Override
-			public void handle(ActionEvent event){
-				System.out.println("Hello Guys");
-			}
-		});
-		VBox root = new VBox();
-		root.getChildren().addAll(btn,exit);
+		
 		try {
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+			Scene scene = new Scene(root);
 			primaryStage.setTitle("Hello Wolrd");
 			primaryStage.setScene(scene);
 			primaryStage.show();
