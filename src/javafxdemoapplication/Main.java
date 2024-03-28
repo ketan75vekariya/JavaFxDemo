db135bf7babd342398f6f09788ae9c9bced5c665
ketan75vekariya <ketan75vekariya@gmail.com>
2024-03-28 10:26:36 +0000

    Events with JavaFX Scene Builder

--- src/javafxdemoapplication/Main.java
+++ src/javafxdemoapplication/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/javafxdemoapplication/Main.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setTitle("Hello Wolrd");
 			primaryStage.setScene(scene);
